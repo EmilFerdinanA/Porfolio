03a803e1a48e0984ca037d91222625e7cee19690
EmilFerdinanA <emilakbar55@gmail.com>
2024-10-10 20:46:23 +0700

    feat: responsive project page

--- app/(sub pages)/projects/page.tsx
+++ app/(sub pages)/projects/page.tsx
@@ -13,9 +13,10 @@ export default function Home() {
         alt="Background Image"
         className="w-full h-full top-0 left-0 object-cover object-center opacity-25 -z-50 fixed"
       />
+
       <ProjectList projects={projectsData} />
 
-      <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
+      <div className="-z-10 flex items-center justify-center fixed top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 left-1/2 lg:-left-24 h-screen">
         <RenderModel>
           <Staff />
         </RenderModel>
--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -4,7 +4,7 @@ import ProjectLayout from "./ProjectLayout";
 
 const ProjectList: React.FC<{ projects: IProject[] }> = ({ projects }) => {
   return (
-    <div className="w-full max-w-4xl px-16 space-y-8 flex flex-col items-center">
+    <div className="w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center">
       {projects.map((project, index) => (
         <ProjectLayout key={index} {...project} />
       ))}
