b33026ab3b7b0df794d1dd7265092281ee43c5fe
EmilFerdinanA <emilakbar55@gmail.com>
2024-10-10 20:37:02 +0700

    feat: responsive about page

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -10,7 +10,7 @@ const ItemLayout: React.FC<ItemLayoutProps> = ({ children, className }) => {
   return (
     <div
       className={clsx(
-        "custom-bg p-8 rounded-xl flex items-center justify-center space-y-8",
+        "custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8",
         className
       )}
     >
@@ -22,12 +22,12 @@ const ItemLayout: React.FC<ItemLayoutProps> = ({ children, className }) => {
 const AboutDetails = () => {
   return (
     <section className="py-20 w-full">
-      <div className="grid grid-cols-12 gap-8 w-full">
-        <ItemLayout className="col-span-8 row-span-2 flex flex-col items-start">
-          <h2 className="text-2xl text-left w-full capitalize">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full">
+        <ItemLayout className="col-span-full lg:col-span-8 row-span-2 flex-col items-start">
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
             Architect of Enchantment
           </h2>
-          <p className="font-light">
+          <p className="font-light text-xs sm:text-sm md:text-base">
             My journey in web development is powered by an array of mystical
             tools and languages, with JavaScript casting the core of my
             enchantments. I wield frameworks like React.js and Next.js with
@@ -40,20 +40,20 @@ const AboutDetails = () => {
           </p>
         </ItemLayout>
 
-        <ItemLayout className="col-span-4 text-accent">
-          <p className="font-semibold w-full text-left text-5xl">
+        <ItemLayout className="col-span-full xs:col-span-6 lg:col-span-4 text-accent">
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
             25+ <sub className="font-semibold text-base">Clients</sub>
           </p>
         </ItemLayout>
 
-        <ItemLayout className="col-span-4 text-accent">
-          <p className="font-semibold w-full text-left text-5xl">
+        <ItemLayout className="col-span-full xs:col-span-6 lg:col-span-4 text-accent">
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
             4+{" "}
             <sub className="font-semibold text-base">Years of experience</sub>
           </p>
         </ItemLayout>
 
-        <ItemLayout className="col-span-4 !p-0">
+        <ItemLayout className="col-span-full sm:col-span-6 md:col-span-4 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-stats.vercel.app/api/top-langs?username=EmilFerdinanA&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&show_icons=true"
@@ -62,7 +62,7 @@ const AboutDetails = () => {
           />
         </ItemLayout>
 
-        <ItemLayout className="col-span-8 !p-0">
+        <ItemLayout className="col-span-full md:col-span-8 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-stats.vercel.app/api?username=EmilFerdinanA&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&show_icons=true"
@@ -80,7 +80,7 @@ const AboutDetails = () => {
           />
         </ItemLayout>
 
-        <ItemLayout className="col-span-6 !p-0">
+        <ItemLayout className="col-span-full md:col-span-6 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-streak-stats.herokuapp.com?user=EmilFerdinanA&theme=dark&hide_border=true&type=png&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B"
@@ -89,7 +89,7 @@ const AboutDetails = () => {
           />
         </ItemLayout>
 
-        <ItemLayout className="col-span-6 !p-0">
+        <ItemLayout className="col-span-full md:col-span-6 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-stats.vercel.app/api/pin/?username=EmilFerdinanA&repo=oasis&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&show_icons=true&description_lines_count=2"
