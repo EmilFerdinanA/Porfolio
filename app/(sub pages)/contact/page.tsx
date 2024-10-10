6ce40fa2b245f31cf76ca55fca27a9c80e006b75
EmilFerdinanA <emilakbar55@gmail.com>
2024-10-10 20:58:36 +0700

    feat: responsive contact page

--- app/(sub pages)/contact/page.tsx
+++ app/(sub pages)/contact/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
         className="w-full h-full top-0 left-0 object-cover object-center opacity-25 -z-50 fixed"
       />
 
-      <article className="relative w-full flex flex-col items-center justify-center space-y-8">
-        <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent text-4xl font-semibold text-center capitalize">
             summon the wizard
           </h1>
 
-          <p className="text-center font-light">
+          <p className="text-center font-light text-sm xs:text-base">
             Step into the circle of enchantment and weave your words into the
             fabric of the cosmos. Whether you seek to conjure collaborations,
             unlock mysteries, or simply share tales of adventure, your messages
