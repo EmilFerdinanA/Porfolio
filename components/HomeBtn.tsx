57d1f6a4497049441fcb4b2ee76fc4059a09c95f
EmilFerdinanA <emilakbar55@gmail.com>
2024-10-15 14:18:36 +0700

    fix: Home button styling

--- components/HomeBtn.tsx
+++ components/HomeBtn.tsx
@@ -13,12 +13,12 @@ const HomeBtn = () => {
       animate={{ scale: 1 }}
       transition={{ delay: 1 }}
       id={"home"}
-      className="text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 left-4 w-fit self-start z-50"
+      className="text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 left-2.5 xs:left-4 w-fit self-start z-50"
       aria-label={"home"}
       href={"/"}
       target={"_self"}
     >
-      <span className="relative w-14 h-14 p-4 hover:text-accent">
+      <span className="relative w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent">
         <Home className="w-full h-auto" strokeWidth={1.5} />
         <span className="peer bg-transparent absolute top-0 left-0 w-full h-full"></span>
         <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
